fb89a5a8a07c5e13d27ecbf81214d4129e0e41ac
nagal <nagal@10.0.0.111>
2025-05-13 14:11:39 -0600

    New line updated

--- src/test/java/com/testNGDemo/NagaTestDemo.java
+++ src/test/java/com/testNGDemo/NagaTestDemo.java
@@ -4,6 +4,7 @@ public class NagaTestDemo {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Nagalaxmitesting192 Branch my branch");
+		System.out.println("Test new");
 
 	}
 
